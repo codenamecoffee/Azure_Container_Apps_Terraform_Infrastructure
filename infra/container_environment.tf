0794c96947033f8f8ee1a72d96d014d8ecf8e530
Gonzalo Rodriguez <gonzalo.rodriguez@endava.com>
2025-09-09 14:49:34 -0300

    add tags to container environment

--- infra/container_environment.tf
+++ infra/container_environment.tf
@@ -3,5 +3,13 @@ resource "azurerm_container_app_environment" "env" {
   location            = var.location
   resource_group_name = var.rg_name
   logs_destination    = "azure-monitor"
+
+  tags = {
+    EndDate      = "30/06/2026"
+    ProjectCode  = "MSDLATAM_UK"
+    ProjectName  = "MVDInternal - Uruguai"
+    ServiceNowID = "RITM0500391"
+  }
+
 }
 
