0df04dcbd0b407644afda6837160da489d3c9857
Gonzalo Rodriguez <gonzalo.rodriguez@endava.com>
2025-09-11 13:26:47 -0300

    fix: add storage account for api storage

---
+++ infra/storage_account.tf
@@ -0,0 +1,15 @@
+resource "azurerm_storage_account" "api_storage" {
+  name                     = "school-of-api-storage"
+  resource_group_name      = var.rg_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    EndDate      = "30/06/2026"
+    ProjectCode  = "MSDLATAM_UK"
+    ProjectName  = "MVDInternal - Uruguai"
+    ServiceNowID = "RITM0500391"
+  }
+
+}
